acb84093133a4e8f2cfee654c2c61d1d12df8d1d
Nick Fisher <manyyearsago@mail.ru>
2022-01-12 23:14:32 +0300

    change test for binary search

--- algorithms/__tests__/binary-search.spec.js
+++ algorithms/__tests__/binary-search.spec.js
@@ -1,13 +1,16 @@
 const { binarySearch } = require("../binary-search");
 
-it('search element in array', () => {
-    expect(binarySearch([1, 4, 6, 13, 16, 21, 29, 55, 70], 4)).toEqual(1);
+describe('binary search test cases', () => {
+    it('search element in array', () => {
+        expect(binarySearch([1, 4, 6, 13, 16, 21, 29, 55, 70], 4)).toEqual(1);
+    });
+    
+    it('search element in array 2', () => {
+        expect(binarySearch([1, 4, 6, 13, 16, 21, 29, 55, 70], 70)).toEqual(8);
+    });
+    
+    it('search element in array 3', () => {
+        expect(binarySearch([1, 4, 6, 13, 16, 21, 29, 55, 70], 56)).toEqual(undefined);
+    });
 });
 
-it('search element in array 2', () => {
-    expect(binarySearch([1, 4, 6, 13, 16, 21, 29, 55, 70], 70)).toEqual(8);
-});
-
-it('search element in array 3', () => {
-    expect(binarySearch([1, 4, 6, 13, 16, 21, 29, 55, 70], 56)).toEqual(undefined);
-});
