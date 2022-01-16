1c07af1ad9e1423aec11fc4e5e03ca3bd94a8945
Nick Fisher <manyyearsago@mail.ru>
2022-01-16 23:35:49 +0300

    add selection sort and utils

---
+++ algorithms/__tests__/selection-sort.spec.js
@@ -0,0 +1,7 @@
+import { selectionSort } from "../selection-sort"
+
+describe('selection sort tests', () => {
+    it('sort [4, 5, 0, 1]', () => {
+        expect(selectionSort([4, 5, 0, 1])).toEqual(expect.arrayContainingExactly([0, 1, 4, 5]))
+    })
+})
