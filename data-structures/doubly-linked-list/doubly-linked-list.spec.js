72f8e2836a74f22f4ea2fbb1063e7f438a3633c6
Nick Fisher <manyyearsago@mail.ru>
2022-01-20 23:50:29 +0300

    add test for doubly linked list

---
+++ data-structures/doubly-linked-list/doubly-linked-list.spec.js
@@ -0,0 +1,8 @@
+describe('Doubly linked list tests', () => {
+    it('create empty linked list', () => {
+        const linkedList = new DoubleLinkedList();
+        expect(linkedList instanceof LinkedList).toBe(true);
+    });
+
+    
+})
