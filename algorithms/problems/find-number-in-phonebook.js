5b1b1a648d164f74a7970f22554a0e9e6105c9fc
Nick Fisher <manyyearsago@mail.ru>
2022-01-21 23:17:54 +0300

    add find number in phoneBook problem

---
+++ algorithms/problems/find-number-in-phonebook.js
@@ -0,0 +1,15 @@
+const phoneBook = [
+    { name: 'Alex Bowman', number: '48-2002' },
+    { name: 'Aric Almirola', number: '10-1001' },
+    { name: 'Bubba Wallace', number: '23-1111' },
+  ];
+
+
+export const findNumberByName = (phoneBook, name) => {
+    let start = 0,
+        end = phoneBook.length - 1;
+  
+        
+}
+
+findNumberByName(phonebook, 'Alex Bowman'); // '48-2002'
