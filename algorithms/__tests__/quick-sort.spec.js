1b1fb759e66168522d4693c3e10db0712773379b
Nick Fisher <manyyearsago@mail.ru>
2022-01-27 23:40:28 +0300

    add tests for quick sort

---
+++ algorithms/__tests__/quick-sort.spec.js
@@ -0,0 +1,11 @@
+const { quickSort } = require("../quick-sort");
+
+it('quickSort empty collection', () => {
+  expect(quickSort([])).toEqual([]);
+});
+
+it('quickSort not change original array', () => {
+  const items = [1, 2, 3, 4, 5];
+  const result = quickSort(items);
+  expect(result).toEqual(items);
+});
